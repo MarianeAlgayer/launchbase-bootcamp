bcd69be8c038a77b186e83e9f79fc4b4879a6b9f
Mariane Algayer <marianealgayer@gmail.com>
2020-06-20 19:51:28 -0300

    Commit challenge 1-4

---
+++ challenge-1-4/banking-operations.js
@@ -0,0 +1,69 @@
+const user = {
+    name: 'Mariane',
+    transactions: [],
+    balance: 0
+}
+
+function createTransaction(transaction) {
+    user.transactions.push(transaction)
+
+    if (transaction.type === 'credit' ) {
+        user.balance += transaction.value
+    } else {
+        user.balance -= transaction.value
+    }
+}
+
+function getHigherTransactionByType(type) {
+    let higherTransaction
+    let higherValue = 0
+
+    for (let transaction of user.transactions) {
+        if (transaction.type == type && higherValue < transaction.value) {
+            higherValue = transaction.value
+            higherTransaction = transaction
+        }
+    }
+
+    return higherTransaction
+}
+
+function getAverageTransactionValue() {
+    let totalValue = 0
+
+    for (let transaction of user.transactions) {
+        totalValue += transaction.value
+    }
+
+    averageValue = totalValue / user.transactions.length
+    return averageValue
+}
+
+function getTransactionsCount() {
+    let count = {
+        credit: 0,
+        debit: 0
+    }
+
+    for (let transaction of user.transactions) {
+        if (transaction.type === 'credit') {
+            count.credit++
+        } else {
+            count.debit++
+        }
+    }
+
+    return count
+}
+
+
+createTransaction({ type: 'credit', value: 50 })
+createTransaction({ type: 'credit', value: 120 })
+createTransaction({ type: 'debit', value: 80 })
+createTransaction({ type: 'debit', value: 30 })
+
+console.log(user.balance)
+console.log(getHigherTransactionByType('credit'))
+console.log(getHigherTransactionByType('debit'))
+console.log(getAverageTransactionValue())
+console.log(getTransactionsCount())
