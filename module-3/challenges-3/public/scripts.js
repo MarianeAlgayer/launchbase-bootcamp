e1b1004f7905b6828f07af838cd25cbd4fe7e340
Mariane Algayer <marianealgayer@gmail.com>
2020-06-29 16:40:41 -0300

    Commit challenge 3-1

---
+++ module-3/challenges-3/public/scripts.js
@@ -0,0 +1,23 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const modal = document.querySelector('.modal')
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards) {
+    card.addEventListener('click', function(){
+        const siteId = card.getAttribute('id')
+        modalOverlay.classList.add('active')
+        modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${siteId}`
+    })
+}
+
+document.querySelector('.close-modal').addEventListener('click', function(){
+    modalOverlay.classList.remove('active')
+
+    if (modal.classList.contains('max')) {
+        modal.classList.remove('max')
+    }
+})
+
+document.querySelector('.max-modal').addEventListener('click', function(){
+    modal.classList.add('max')
+})
