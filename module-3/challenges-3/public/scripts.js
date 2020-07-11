e00587d09dcaefb6b2480cc284da0483bc58c346
Mariane Algayer <marianealgayer@gmail.com>
2020-07-10 21:12:25 -0300

    Commit challenge 3-3

--- module-3/challenges-3/public/scripts.js
+++ module-3/challenges-3/public/scripts.js
@@ -1,3 +1,5 @@
+// access course-description page
+
 const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
@@ -6,3 +8,18 @@ for (let card of cards) {
         window.location.href = `/courses/${siteId}`
     })
 }
+
+// responsive menu
+
+let show = true;
+
+const header = document.querySelector('header')
+const menuToggle = header.querySelector('.menu-toggle')
+
+menuToggle.addEventListener('click', function() {
+
+    document.body.style.overflow = show ? 'hidden' : 'initial'
+
+    header.classList.toggle('on', show)
+    show = !show
+})
